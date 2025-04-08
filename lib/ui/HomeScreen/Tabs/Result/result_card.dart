fb60d09939a8e0905af10b177d151da0fe87fb3c
SillyGoose123 <mail@sillygoose123.de>
2025-04-08 18:24:55 +0200

    #1 Bug fixes

--- lib/ui/HomeScreen/Tabs/Result/result_card.dart
+++ lib/ui/HomeScreen/Tabs/Result/result_card.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
-import 'package:shootbook/ui/HomeScreen/Tabs/Result/shot_view.dart';
 import '../../../../models/shooting/result.dart';
+import 'ShotView/shot_view.dart';
 
 class ResultCard extends StatelessWidget {
   final Result result;
@@ -9,25 +9,27 @@ class ResultCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(padding: EdgeInsets.all(5), child: Row(
-      mainAxisAlignment: MainAxisAlignment.start,
-      spacing: 10,
-      children: [
-        SizedBox(height: 100, width: 100, child: ShotView(result: result)),
-        Column(
+    return Padding(
+        padding: EdgeInsets.all(5),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          spacing: 10,
           children: [
-            Text(result.type.toText()),
-            Text(result.formatTime()),
-            Row(
-              spacing: 10,
+            SizedBox(height: 100, width: 100, child: ShotView(result: result)),
+            Column(
               children: [
-                Text(result.calcNonTenthValue().toString()),
-                Text(result.value.toString()),
+                Text(result.type.toText()),
+                Text(result.formatTime()),
+                Row(
+                  spacing: 10,
+                  children: [
+                    Text(result.calcNonTenthValue().toString()),
+                    Text(result.value.toString()),
+                  ],
+                ),
               ],
-            ),
+            )
           ],
-        )
-      ],
-    ));
+        ));
   }
 }
